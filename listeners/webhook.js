ddb8e47b22632863601aeef1b9fe32d68865889c
Joaquin Sanchez <jsanchez@slingr.io>
2025-01-03 16:38:11 -0300

    Adding google drive default listener

---
+++ listeners/webhook.js
@@ -0,0 +1,20 @@
+/****************************************************
+ Listeners
+ ****************************************************/
+
+ listeners.defaultWebhookGoogleDrive = {
+    label: 'Catch HTTP google drive events',
+    type: 'service',
+    options: {
+        service: 'http',
+        event: 'webhook',
+        matching: {
+            path: '/googledrive',
+        }
+    },
+    callback: function(event) {
+        sys.logs.info('[googledrive] Received Google Drive webhook. Processing and triggering a package event.', event);
+        sys.logs.info('[googledrive] Triggering google drive event [webhook]');
+        sys.events.triggerEvent('googledrive:webhook', event.data);
+    }
+};
